9af31bd6ab2e5269d93f5abdf919e8fc54157d4e
gxlpes <guilhermxlopes@gmail.com>
2024-08-17 15:12:10 -0300

    fix: remove context

--- src/main/java/com/ru/scraper/RuScraperApiApplication.java
+++ src/main/java/com/ru/scraper/RuScraperApiApplication.java
@@ -1,6 +1,5 @@
 package com.ru.scraper;
 
-import com.amazonaws.services.lambda.runtime.Context;
 import com.ru.scraper.service.ScrapService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,16 +7,12 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.function.Function;
 
-
-
 @SpringBootApplication
 public class RuScraperApiApplication {
     private final ScrapService scrapService;
-    private Context context;
 
-    public RuScraperApiApplication(ScrapService scrapService, Context context) {
+    public RuScraperApiApplication(ScrapService scrapService) {
         this.scrapService = scrapService;
-        this.context = context;
     }
 
     public static void main(String[] args) {
@@ -28,7 +23,6 @@ public class RuScraperApiApplication {
     public Function<?, ?> scraperMenu() {
         return (input) -> {
             try {
-                System.out.println("CONTEXT HERE:" + context);
                 System.out.println("INPUT RECEIVED: " + input.toString());
                 return scrapService.scrape();
             } catch (Exception e) {
